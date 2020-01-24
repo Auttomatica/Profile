7e04d236c8f22b9ea332a5c22f2f0322b7ed21fc
Carmela <carme.rl@hotmail.com>
2020-01-24 16:08:59 -0300

    primeros pasos

---
+++ JS/app.js
@@ -0,0 +1,55 @@
+
+
+const misProyectos = document.querySelector('.mis-proyectos');
+const menu = document.getElementById('menuPrincipal');
+const focoEnMisProyectos = document.getElementById('proyectos');
+
+eventListener();
+
+
+function eventListener(){
+
+misProyectos.addEventListener('click', verMisProyectos);
+//menu.addEventListener('click', verOpcionesMenu);
+
+}
+
+
+// DELEGATION PARA MIS PROYECTOS
+function verMisProyectos(e){
+
+    e.preventDefault();
+
+   if(e.target.classList.contains('verProyecto')){
+       
+   }
+
+    ;
+
+
+
+
+
+}
+
+
+ //function verOpcionesMenu(e){
+
+   // e.preventDefault();
+
+    
+   // if(e.target.classList.contains('btnMisProyectos')){
+
+   //     document.getElementById('proyectos').focus();
+
+  //  }
+
+
+
+//}
+
+function getFocus(){
+
+    document.getElementById('proyectos').focus();
+
+}
