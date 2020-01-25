49fc140248b7ea38db0b42e6ddf42f4ad21ae0dc
Carmela <carme.rl@hotmail.com>
2020-01-25 12:32:38 -0300

    section focus

--- JS/app.js
+++ JS/app.js
@@ -1,55 +1,23 @@
-
-
 const misProyectos = document.querySelector('.mis-proyectos');
 const menu = document.getElementById('menuPrincipal');
 const focoEnMisProyectos = document.getElementById('proyectos');
 
 eventListener();
 
+function eventListener() {
 
-function eventListener(){
-
-misProyectos.addEventListener('click', verMisProyectos);
-//menu.addEventListener('click', verOpcionesMenu);
+    misProyectos.addEventListener('click', verMisProyectos);
+    menu.addEventListener('click', verOpcionesMenu);  
 
 }
 
-
 // DELEGATION PARA MIS PROYECTOS
-function verMisProyectos(e){
+function verMisProyectos(e) {
 
     e.preventDefault();
 
-   if(e.target.classList.contains('verProyecto')){
-       
-   }
+    if (e.target.classList.contains('verProyecto')) {
 
-    ;
-
-
-
-
-
-}
-
-
- //function verOpcionesMenu(e){
-
-   // e.preventDefault();
-
-    
-   // if(e.target.classList.contains('btnMisProyectos')){
-
-   //     document.getElementById('proyectos').focus();
-
-  //  }
-
-
-
-//}
-
-function getFocus(){
-
-    document.getElementById('proyectos').focus();
+    };
 
 }
